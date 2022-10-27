b6e5bd7d1c3751d90f259e54a96befec6cfb4ab3
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-10-27 07:06:00 -0600

    spacing

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const emailField = document.getElementById("email");
 const response = document.getElementById("response");
 const button = document.getElementById("submit-button");
 
-button.addEventListener('click', function () {
-    const email = emailField.value;
-    if (validateEmail(email)) {
-        response.innerHTML = '';
-      } else {
-        response.innerHTML = 'Please provide a valid email address.';
-      }
-    });
- 
-  function validateEmail(email) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+button.addEventListener("click", function () {
+	const email = emailField.value;
+	if (validateEmail(email)) {
+		response.innerHTML = "";
+	} else {
+		response.innerHTML = "Please provide a valid email address.";
+	}
+});
+
+function validateEmail(email) {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
